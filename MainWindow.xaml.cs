f3c6802d027f7efbc778f75ccaa2fd9472b2ec8f
yonah <yonahweiner@gmail.com>
2025-11-23 15:21:54 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace ShabbatMovieLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindowVM ViewModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new MainWindowVM();
+        }
+    }
+}
---
+++ MainWindowVM.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ShabbatMovieLauncher.Services;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace ShabbatMovieLauncher
+{
+    public class MainWindowVM : ObservableObject
+    {
+        public ICommand SetScheduleButton { get; set; }
+        private PCWaker pCWaker = new PCWaker();
+        public MainWindowVM()
+        {
+           SetScheduleButton = new RelayCommand(ExecuteSetScheduleButton);
+        }
+
+        private void ExecuteSetScheduleButton()
+        {
+            MessageBox.Show($"PC will wake up at {pCWaker.WakeupSchedule}");
+        }
+    }
+}
